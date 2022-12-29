8b69c2f18155b28efd302dfafbb91ccf8fdaee03
sh-navid <shamshirsaz.navid@live.com>
2022-12-29 05:35:07 +0330

    Update

---
+++ concepts/ddl/TP03_01.sql
@@ -0,0 +1,22 @@
+-- --------------------------------------------------------------------------
+-- Keys
+
+-- || 1 || Primary Key (PK) constraint: (https://byjus.com/gate/types-of-keys-in-dbms/)
+-- || The primary key refers to a column or a set of columns of a table 
+-- || that helps us identify all the records uniquely present in that 
+-- || table. A table can consist of just one primary key. Also, this 
+-- || primary key cannot consist of the same values repeating for any of 
+-- || its rows.
+-- || The PK (PRIMARY KEY) constraint that we put on a column/set of 
+-- || columns won’t allow these to have a null value or a duplicate. 
+-- || Any table can consist of only a single primary key constraint.
+
+-- || 2 || Foreign Key: (https://byjus.com/gate/types-of-keys-in-dbms/)
+-- || use a foreign key to establish relationships between two available tables.
+
+-- || 3 || Unique Key: (https://byjus.com/gate/types-of-keys-in-dbms/)
+-- || refers to a column/a set of columns that identify every record uniquely in 
+-- || a table. All the values in this key would have to be unique. It is because 
+-- || it is only capable of having one null value. A primary key, on the other 
+-- || hand, cannot have a null value.
+-- --------------------------------------------------------------------------
