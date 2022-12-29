45a6d3f60c748d83ca750108fda8f3c539d94993
sh-navid <shamshirsaz.navid@live.com>
2022-12-29 06:51:53 +0330

    Update

--- concepts/ddl/TP03_04.sql
+++ concepts/ddl/TP03_04.sql
@@ -4,6 +4,8 @@
 
 -- --------------------------------------------------------------------------
 -- First normal form (1NF): (https://www.geeksforgeeks.org/first-normal-form-1nf/)
+--                          (https://www.freecodecamp.org/news/database-normalization-1nf-2nf-3nf-table-examples/)
+-- -- -- [eliminates repeating groups]
 -- -- 1 -> There are only single-valued Attributes.
 -- -- 2 -> Attribute Domain does not change.
 -- -- 3 -> There is a unique name for every Attribute/Column.
@@ -13,6 +15,7 @@
 -- ID      Name       Author
 -- 01      Book1      Author1, Author2, Author3  <- [Multi-Valued]
 
+-- --------------------------------
 -- Books (1NF)
 -- ID      Name       Author
 -- 01      Book1      Author1  
@@ -34,7 +37,9 @@
  
 -- --------------------------------------------------------------------------
 -- Second normal form (2NF): (https://www.tutorialspoint.com/Second-Normal-Form-2NF)
--- -- 1 -> A table is in 2NF, only if a relation is in 1NF and meet all the rules
+--                           (https://www.freecodecamp.org/news/database-normalization-1nf-2nf-3nf-table-examples/)
+-- -- -- [eliminates redundancy, partial functional dependency]
+-- -- 1 -> It’s already in 1NF
 -- -- 2 -> And every non-key attribute is fully dependent on primary key.
 
 -- Books
@@ -66,7 +71,10 @@
 -- 01       02
 -- 01       03
 
--- Third normal form (3NF):
+-- Third normal form (3NF): (https://www.freecodecamp.org/news/database-normalization-1nf-2nf-3nf-table-examples/)
+-- -- 1 -> It’s already in 2NF
+-- -- 2 -> Have no transitive partial dependency.
+
 -- Boyce-Codd normal form (BCNF):
 -- Fourth normal form (4NF):
 -- Fifth normal form (5NF):
