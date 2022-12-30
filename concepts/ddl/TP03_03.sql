e2a689d5e793b00778d5b399801366684e9be7a7
sh-navid <shamshirsaz.navid@live.com>
2022-12-30 07:27:18 +0330

    Update

--- concepts/ddl/TP03_03.sql
+++ concepts/ddl/TP03_03.sql
@@ -36,3 +36,68 @@ INSERT INTO Users VALUES();
 INSERT INTO Users VALUES();
 INSERT INTO Users VALUES(NULL,"A","A"),(NULL,"B","B"),(NULL,"C","C"); -- To insert multiple values
 SELECT * FROM Users;
+
+
+-- --------------------------------------------------------------------------
+-- Constraints - Check
+-- --------------------------------------------------------------------------
+
+DROP   TABLE IF EXISTS Users;
+CREATE TABLE Users( 
+    ID          INTEGER unsigned    PRIMARY KEY auto_increment, 
+    First       VARCHAR(30),
+    Last        VARCHAR(30),
+    CONSTRAINT check_name_len CHECK (LENGTH(First)>2 AND LENGTH(Last)>2)     
+);
+
+INSERT INTO Users VALUES(NULL, "Ali", "Ali");
+INSERT INTO Users VALUES(NULL, "Al", "Al");
+SELECT * FROM Users;
+
+
+-- --------------------------------------------------------------------------
+-- Constraints - FOREIGN Key
+-- --------------------------------------------------------------------------
+DROP   TABLE IF EXISTS Orders;
+DROP   TABLE IF EXISTS Gender;
+DROP   TABLE IF EXISTS Users;
+
+DROP   TABLE IF EXISTS Gender;
+CREATE TABLE Gender( 
+    ID          INTEGER unsigned    PRIMARY KEY, 
+    Value       VARCHAR(30)
+);
+
+DROP   TABLE IF EXISTS Users;
+CREATE TABLE Users( 
+    ID          INTEGER unsigned    PRIMARY KEY auto_increment, 
+    First       VARCHAR(30),
+    Last        VARCHAR(30),
+    GenderID    INTEGER unsigned,
+    FOREIGN KEY (GenderID) REFERENCES Gender(ID) 
+);
+
+DROP   TABLE IF EXISTS Orders;
+CREATE TABLE Orders( 
+    ID          INTEGER unsigned    PRIMARY KEY auto_increment, 
+    Value       VARCHAR(30),
+    UserID      INTEGER unsigned,
+    FOREIGN KEY (UserID) REFERENCES Users(ID) ON DELETE CASCADE ON UPDATE CASCADE
+);
+
+INSERT INTO Gender VALUES(1, "M");
+INSERT INTO Gender VALUES(2, "F");
+
+INSERT INTO Users VALUES(NULL, "Ali", "Ali",1);
+INSERT INTO Users VALUES(NULL, "Lale", "Lale",2);
+
+INSERT INTO Orders VALUES(NULL, "Cake", 2);
+INSERT INTO Orders VALUES(NULL, "Flower", 2);
+INSERT INTO Orders VALUES(NULL, "Soda", 1);
+INSERT INTO Orders VALUES(NULL, "Phone", 1);
+
+SELECT * FROM Users;
+SELECT * FROM Orders;
+DELETE FROM Users WHERE ID=1;
+SELECT * FROM Users;
+SELECT * FROM Orders;
