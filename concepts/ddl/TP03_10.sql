3770689f6cf87ccd22bec0426d0442358eb15319
sh-navid <shamshirsaz.navid@live.com>
2022-12-30 06:56:50 +0330

    Update

--- concepts/ddl/TP03_10.sql
+++ concepts/ddl/TP03_10.sql
@@ -1,3 +1,17 @@
 -- --------------------------------------------------------------------------
--- Speak about cloth store
+-- AUTO_INCREMENT from another value
 -- --------------------------------------------------------------------------
+DROP TABLE IF EXISTS Test;
+CREATE TABLE Test(
+    ID      INT PRIMARY KEY AUTO_INCREMENT,
+    Val     VARCHAR(200),
+    F       FLOAT
+);
+
+ALTER TABLE Test AUTO_INCREMENT=2000; 
+
+INSERT INTO Test VALUES(NULL,"Hello",14);
+INSERT INTO Test VALUES(NULL,"Goodbye",25.00001);
+INSERT INTO Test VALUES(NULL,"Hi",25.00001);
+
+SELECT * FROM Test;
