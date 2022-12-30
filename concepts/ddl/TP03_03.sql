c91bd74a0305d5742eadf54ee10cd88b59abfa81
sh-navid <shamshirsaz.navid@live.com>
2022-12-30 07:10:50 +0330

    Update

--- concepts/ddl/TP03_03.sql
+++ concepts/ddl/TP03_03.sql
@@ -1,5 +1,5 @@
 -- --------------------------------------------------------------------------
--- Constraints
+-- Constraints - Primary Key
 -- --------------------------------------------------------------------------
 
 DROP   TABLE IF EXISTS Users;
@@ -14,4 +14,25 @@ CREATE TABLE Users(
 INSERT INTO Users VALUES(NULL,"Ali","Alizad");
 INSERT INTO Users VALUES(NULL,"Ali","Alizade");
 INSERT INTO Users VALUES(NULL,"Ali","Alizade");
+SELECT * FROM Users;
+
+
+-- --------------------------------------------------------------------------
+-- Constraints - Default
+-- --------------------------------------------------------------------------
+
+DROP   TABLE IF EXISTS Users;
+CREATE TABLE Users( 
+    ID          INTEGER unsigned    PRIMARY KEY auto_increment, 
+    First       VARCHAR(30)         NOT NULL    default "NOT SET", 
+    Last        VARCHAR(30)         NOT NULL    default "NOT SET"
+);
+
+INSERT INTO Users VALUES(NULL,"Ali",DEFAULT);
+INSERT INTO Users VALUES(NULL,DEFAULT ,DEFAULT);
+INSERT INTO Users VALUES(NULL,"Ali","Alizade");
+INSERT INTO Users VALUES();
+INSERT INTO Users VALUES();
+INSERT INTO Users VALUES();
+INSERT INTO Users VALUES(NULL,"A","A"),(NULL,"B","B"),(NULL,"C","C"); -- To insert multiple values
 SELECT * FROM Users;
--- concepts/ddl/TP03_11.sql
+++ concepts/ddl/TP03_11.sql
@@ -1,3 +1,23 @@
 -- --------------------------------------------------------------------------
--- Speak about cloth store
+-- Index
 -- --------------------------------------------------------------------------
+DROP TABLE IF EXISTS Test;
+CREATE TABLE Test(
+    ID      INT PRIMARY KEY AUTO_INCREMENT,
+    Val     VARCHAR(200),
+    F       FLOAT
+);
+
+CREATE INDEX val_idx ON Test(Val);
+
+ALTER TABLE Test DROP INDEX val_idx;
+
+CREATE UNIQUE INDEX val_idx ON Test(Val);
+
+
+-- --------------------------------------------------------------------------
+-- About Indexes
+-- --------------------------------------------------------------------------
+-- Indexes let search for data faster
+-- However indexes themselves need to update
+-- So just put index on needed columns for performance
