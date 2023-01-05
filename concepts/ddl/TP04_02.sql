dfa14b0b77f6c32991778833c9af4b8755fa3359
sh-navid <shamshirsaz.navid@live.com>
2023-01-05 06:19:44 +0330

    Update

--- concepts/ddl/TP04_02.sql
+++ concepts/ddl/TP04_02.sql
@@ -7,7 +7,6 @@ DROP TABLE IF EXISTS Author;
 CREATE TABLE IF NOT EXISTS Author(
     ID              INTEGER         NOT NULL    UNIQUE      AUTO_INCREMENT,
     Name            VARCHAR(100)    NOT NULL,
-
     PRIMARY KEY (ID)
 );
 
@@ -15,17 +14,17 @@ CREATE TABLE IF NOT EXISTS Author(
 CREATE TABLE IF NOT EXISTS Book(
     ID              INTEGER         NOT NULL    UNIQUE      AUTO_INCREMENT,
     Title           VARCHAR(100)    NOT NULL,
-    Author_ID    INTEGER             NULL,
+    Author_ID       INTEGER             NULL,
 
     PRIMARY KEY (ID),
-    FOREIGN KEY (Author_ID)  REFERENCES  Author(ID), -- ON DELETE CASCADE ON UPDATE CASCADE,
+    FOREIGN KEY (Author_ID)  REFERENCES  Author(ID) -- ON DELETE CASCADE ON UPDATE CASCADE,
 );
 
 
 
-INSERT INTO Author    VALUES (1,     "Alireza");
-INSERT INTO Author    VALUES (2,     "Saeed");
-INSERT INTO Author    VALUES (50,    "Majid");
+INSERT INTO Author    VALUES (1,    "Alireza");
+INSERT INTO Author    VALUES (2,    "Saeed");
+INSERT INTO Author    VALUES (50,   "Majid");
 
 INSERT INTO Book      VALUES (NULL, "Book1", 1);
 INSERT INTO Book      VALUES (NULL, "Book2", 1);
@@ -35,5 +34,46 @@ INSERT INTO Book      VALUES (NULL, "Book5", NULL);
 
 
 -- --------------------------------------------------------
--- Left JOIN
+-- INNER JOIN
 -- --------------------------------------------------------
+SELECT Book.Title, Author.Name 
+FROM Book
+INNER JOIN Author
+ON Book.Author_ID = Author.ID;
+
+-- --------------------------------------------------------
+-- LEFT JOIN
+-- --------------------------------------------------------
+SELECT Book.Title, Author.Name 
+FROM Book
+LEFT JOIN Author
+ON Book.Author_ID = Author.ID;
+
+-- --------------------------------------------------------
+-- RIGHT JOIN
+-- --------------------------------------------------------
+SELECT Book.Title, Author.Name 
+FROM Book
+RIGHT JOIN Author
+ON Book.Author_ID = Author.ID;
+
+-- --------------------------------------------------------
+-- FULL OUTER JOIN
+-- --------------------------------------------------------
+SELECT Book.Title, Author.Name 
+FROM Book
+LEFT JOIN Author
+ON Book.Author_ID = Author.ID
+UNION
+SELECT Book.Title, Author.Name 
+FROM Book
+RIGHT JOIN Author
+ON Book.Author_ID = Author.ID;
+
+-- --------------------------------------------------------
+-- CROSS JOIN - All Records From All Tables
+-- --------------------------------------------------------
+SELECT Book.Title, Author.Name 
+FROM Book
+CROSS JOIN Author
+ON Book.Author_ID = Author.ID
