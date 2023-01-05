da44ec3c9eba3574a1841fe438a9aa7be8197d12
sh-navid <shamshirsaz.navid@live.com>
2023-01-05 06:46:53 +0330

    Update

--- concepts/ddl/TP04_02.sql
+++ concepts/ddl/TP04_02.sql
@@ -25,6 +25,7 @@ CREATE TABLE IF NOT EXISTS Book(
 INSERT INTO Author    VALUES (1,    "Alireza");
 INSERT INTO Author    VALUES (2,    "Saeed");
 INSERT INTO Author    VALUES (50,   "Majid");
+INSERT INTO Author    VALUES (60,   NULL);
 
 INSERT INTO Book      VALUES (NULL, "Book1", 1);
 INSERT INTO Book      VALUES (NULL, "Book2", 1);
@@ -76,4 +77,43 @@ ON Book.Author_ID = Author.ID;
 SELECT Book.Title, Author.Name 
 FROM Book
 CROSS JOIN Author
-ON Book.Author_ID = Author.ID
+ON Book.Author_ID = Author.ID;
+
+
+-- --------------------------------------------------------
+-- CROSS JOIN - All Records From All Tables
+-- --------------------------------------------------------
+SELECT Book.Title, Author.Name 
+FROM Book
+CROSS JOIN Author
+ON Book.Author_ID = Author.ID;
+
+
+-- --------------------------------------------------------
+-- SELF JOIN - NOT A GOOD EXAMPLE
+-- --------------------------------------------------------
+DROP TABLE IF EXISTS T;
+CREATE TABLE IF NOT EXISTS T( -- WARNING A COMPLETE BAD PRACTICE
+    ID              INTEGER         NOT NULL    UNIQUE      AUTO_INCREMENT,
+    Student         VARCHAR(100)    DEFAULT NULL,
+    Teacher         VARCHAR(100)    DEFAULT NULL,
+    PRIMARY KEY (ID)
+);
+INSERT INTO T VALUES(NULL,"S1","T1");
+INSERT INTO T VALUES(NULL,"S2","T1");
+INSERT INTO T VALUES(NULL,"S3","T1");
+INSERT INTO T VALUES(NULL,"S1","T2");
+INSERT INTO T VALUES(NULL,"S2","T2");
+INSERT INTO T VALUES(NULL,"S4",DEFAULT);
+INSERT INTO T VALUES(NULL,DEFAULT,"T3");
+
+-- SELECT DISTINCT T1.Student, T2.Teacher
+-- FROM T AS T1
+-- LEFT JOIN T AS T2
+-- ON T1.ID = T2.ID
+-- ORDER BY T1.Student, T2.Teacher;
+
+SELECT DISTINCT T1.Student, T2.Teacher
+FROM T AS T1, T AS T2
+WHERE T1.ID <> T2.ID
+ORDER BY T1.Student, T2.Teacher;
