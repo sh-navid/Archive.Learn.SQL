7b28daca522ed7239e13c61204bf18b75ba045af
sh-navid <shamshirsaz.navid@live.com>
2023-01-05 05:21:34 +0330

    Update

--- concepts/ddl/TP04_01.sql
+++ concepts/ddl/TP04_01.sql
@@ -1,17 +1,60 @@
-DROP DATABASE IF EXISTS test;
-CREATE DATABASE test;
+-- --------------------------------------------------------
+-- Drop/Create Database
+-- --------------------------------------------------------
+DROP DATABASE IF EXISTS Test;
+CREATE DATABASE Test;
 
-DROP TABLE IF EXISTS record;
-CREATE TABLE record(
+-- --------------------------------------------------------
+-- Drop/Create Table
+-- --------------------------------------------------------
+DROP TABLE IF EXISTS Record;
+CREATE TABLE Record(
     ID      INT NOT NULL UNIQUE PRIMARY KEY AUTO_INCREMENT,
-    VALUE   TEXT
+    Value   VARCHAR(50)
 );
 
-INSERT INTO record VALUES(NULL,"Hello");
-INSERT INTO record VALUES(NULL,"Hello");
+-- --------------------------------------------------------
+-- Insert data
+-- --------------------------------------------------------
+INSERT INTO Record VALUES(NULL,"Hello");
+INSERT INTO Record VALUES(NULL,"Hello");
 
-ALTER TABLE record AUTO_INCREMENT=1000;
+-- --------------------------------------------------------
+-- Alter
+-- --------------------------------------------------------
+ALTER TABLE Record AUTO_INCREMENT=1000;
+ALTER TABLE Record ADD Value2 VARCHAR(50);
 
-INSERT INTO record VALUES(NULL,"Goodbye");
-SELECT * FROM record;
-SELECT DISTINCT(VALUE) FROM record;
+ALTER TABLE Record ADD Value3 VARCHAR(50);
+ALTER TABLE Record DROP COLUMN Value3;
+
+-- --------------------------------------------------------
+-- SELECT DISTINCT
+-- --------------------------------------------------------
+INSERT INTO Record VALUES(NULL,"Goodbye","");
+SELECT * FROM Record;
+SELECT DISTINCT(Value) FROM Record;
+
+-- --------------------------------------------------------
+-- Constraint (Create Index)
+-- --------------------------------------------------------
+-- ALTER TABLE Record DROP INDEX my_index_2;
+-- CREATE UNIQUE INDEX my_index_2 ON Record(Value);
+CREATE INDEX my_index_2 ON Record(Value);
+
+-- --------------------------------------------------------
+-- Composite Primary Key 
+-- --------------------------------------------------------
+DROP TABLE IF EXISTS Record;
+CREATE TABLE Record(
+    V1      VARCHAR(50),
+    V2      VARCHAR(50),
+    PRIMARY KEY(V1,V2)
+);
+
+-- ALTER TABLE Record ADD PRIMARY KEY (V1,V2);
+DESCRIBE Record;
+
+INSERT INTO Record VALUES ("H","H");
+INSERT INTO Record VALUES ("H","H");
+SELECT * FROM Record;
